1700a22f67b8b65c06c8bde3b1108587896e17bf
Ahmed Sabry Mohamed Wahba <ahmeddood7@gmail.com>
2020-12-08 20:13:42 +0200

    adding the initial structure of the project

---
+++ src/main/java/com/pocket/pos/PocketPosApplication.java
@@ -0,0 +1,13 @@
+package com.pocket.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketPosApplication.class, args);
+	}
+
+}
