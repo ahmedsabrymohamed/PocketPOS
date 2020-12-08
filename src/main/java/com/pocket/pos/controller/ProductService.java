8162c85fb32671d0b01249b44a66f1887ef0ef1e
Ahmed Sabry Mohamed Wahba <ahmeddood7@gmail.com>
2020-12-08 20:14:47 +0200

    adding the basic services and basic API sturcture

---
+++ src/main/java/com/pocket/pos/controller/ProductService.java
@@ -0,0 +1,12 @@
+package com.pocket.pos.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/Products")
+public class ProductService {
+
+}
