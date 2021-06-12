d87752c22148158224c5594ea7a204170f3b87f8
AhmedSabryMohamed <ahmeddood7@gmail.com>
2021-06-12 11:49:29 +0200

    add API error and SimpleResponse

--- src/main/java/com/pocket/pos/exception/ApiError.java
+++ src/main/java/com/pocket/pos/exception/ApiError.java
@@ -7,26 +7,18 @@ import org.springframework.http.HttpStatus;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class ApiSimpleResponse {
+public class ApiError {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     public LocalDateTime timestamp;
     public HttpStatus status;
     public String message;
     public List<String> errors;
-	public ApiSimpleResponse( HttpStatus status, String message, List<String> errors) {
+	public ApiError(LocalDateTime timestamp, HttpStatus status, String message, List<String> errors) {
 		super();
-		this.timestamp = LocalDateTime.now();
+		this.timestamp = timestamp;
 		this.status = status;
 		this.message = message;
 		this.errors = errors;
-	}
-	
-	public ApiSimpleResponse(HttpStatus status, String message) {
-		super();
-		this.timestamp = LocalDateTime.now();
-		this.status = status;
-		this.message = message;
-		this.errors = null;
 	}
     
     
